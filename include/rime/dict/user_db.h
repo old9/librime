e3311127567acf969ca0fb2a2b8d3472ca515e9a
lotem <chen.sst@gmail.com>
2014-12-03 20:38:57 +0800

    refactor user_db.
    commit dbfc546f9e448eb3c611e38416504d0fb1abdc98
    Author: lotem <chen.sst@gmail.com>
    Date:   Wed Dec 3 17:56:13 2014 +0800
    
        compile!
    
    commit 3272bf78a8ab88168763212f9646a3764434c541
    Author: lotem <chen.sst@gmail.com>
    Date:   Wed Dec 3 16:48:49 2014 +0800
    
        almost.
    
    commit e50590ba679c4a08a99de3a2d94d50abf9542c43
    Author: lotem <chen.sst@gmail.com>
    Date:   Fri Nov 21 19:34:33 2014 +0800
    
        temp.
    
    commit f441c2eb4d6d67a1cf77bd896965e469c94f9faf
    Author: lotem <chen.sst@gmail.com>
    Date:   Mon Sep 15 20:12:37 2014 +0800
    
        temp.

--- include/rime/dict/user_db.h
+++ include/rime/dict/user_db.h
@@ -8,7 +8,10 @@
 #define RIME_USER_DB_H_
 
 #include <stdint.h>
+#include <functional>
 #include <string>
+#include <rime/component.h>
+#include <rime/dict/db.h>
 #include <rime/dict/db_utils.h>
 
 namespace rime {
@@ -27,22 +30,88 @@ struct UserDbValue {
   bool Unpack(const std::string& value);
 };
 
-template <class BaseDb>
-class UserDb : public BaseDb {
+class UserDb {
  public:
-  explicit UserDb(const std::string& name);
+  class Component : public Db::Component {
+   public:
+    virtual std::string extension() const = 0;
+    virtual std::string snapshot_extension() const = 0;
+  };
 
-  virtual bool CreateMetadata();
-  virtual bool Backup(const std::string& snapshot_file);
-  virtual bool Restore(const std::string& snapshot_file);
+  static Component* Require(const std::string& name) {
+    return static_cast<Component*>(Db::Require(name));
+  }
+
+  UserDb() = delete;
+};
+
+
+class UserDbHelper {
+ public:
+  UserDbHelper(Db* db) : db_(db) {
+  }
+  UserDbHelper(const unique_ptr<Db>& db) : db_(db.get()) {
+  }
+  UserDbHelper(const shared_ptr<Db>& db) : db_(db.get()) {
+  }
+
+  bool UpdateUserInfo();
+  bool UniformBackup(const std::string& snapshot_file,
+                     std::function<bool ()> fallback);
+  bool UniformRestore(const std::string& snapshot_file,
+                      std::function<bool ()> fallback);
 
   bool IsUserDb();
   std::string GetDbName();
   std::string GetUserId();
   std::string GetRimeVersion();
 
+ protected:
+  Db* db_;
+};
+
+template <class BaseDb>
+class UserDbWrapper : public BaseDb {
+ public:
+  UserDbWrapper(const std::string& db_name);
+
+  virtual bool CreateMetadata() {
+    return BaseDb::CreateMetadata() &&
+        UserDbHelper(this).UpdateUserInfo();
+  }
+  virtual bool Backup(const std::string& snapshot_file) {
+    return UserDbHelper(this).UniformBackup(snapshot_file,
+        [&]() -> bool {
+          return BaseDb::Backup(snapshot_file);
+        });
+  }
+  virtual bool Restore(const std::string& snapshot_file) {
+    return UserDbHelper(this).UniformRestore(snapshot_file,
+        [&]() -> bool {
+          return BaseDb::Restore(snapshot_file);
+        });
+  }
+};
+
+template <class BaseDb>
+struct UserDbFormat {
   static const std::string extension;
   static const std::string snapshot_extension;
+};
+
+template <class BaseDb>
+class UserDbComponent : public UserDb::Component {
+ public:
+  virtual Db* Create(const std::string& name) {
+    return new UserDbWrapper<BaseDb>(name + extension());
+  }
+
+  virtual std::string extension() const {
+    return UserDbFormat<BaseDb>::extension;
+  }
+  virtual std::string snapshot_extension() const {
+    return UserDbFormat<BaseDb>::snapshot_extension;
+  }
 };
 
 class UserDbMerger : public Sink {
